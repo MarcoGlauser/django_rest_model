75c2c5870a05aea90251cec8c0316c1bed867043
Marco Glauser <marco.glauser@apgsga.ch>
2016-11-18 14:50:51 +0100

    initial setup

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # django_rest_model
+Django Rest Model let's you consume Django Rest Framework Services as if they were Django ORM Models
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='django_rest_model',
+    version='0.1.0',
+    packages=['django_rest_model'],
+    url='',
+    license='MIT ',
+    author='MarcoGlauser',
+    author_email='',
+    description='',
+    setup_requires=['pytest-runner',],
+    tests_require=['pytest',],
+    install_requires=[
+        'Django>=1.8',
+        'requests>=2.12',
+        'djangorestframework>=3.5',
+        'djangorestframework-yaml',
+        'djangorestframework-xml'
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Development Status :: 4 - Beta',
+    ]
+)
