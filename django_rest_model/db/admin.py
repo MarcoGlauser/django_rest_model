3bf85c57737e488b22b4c9413d382d681edb7f7d
MarcoGlauser <marco.glauser@apgsga.ch>
2016-11-18 15:22:29 +0100

    added admin elements

---
+++ django_rest_model/db/admin.py
@@ -0,0 +1,13 @@
+from django.contrib.admin import ModelAdmin, StackedInline, TabularInline
+
+
+class DjangoRestModelAdmin(ModelAdmin):
+    pass
+
+
+class DjangoRestModelStackedInline(StackedInline):
+    pass
+
+
+class DjangoRestModelTabularInline(TabularInline):
+    pass
